19f521848d714d307ff8510908805324586e7c32
Kenji Fleming <kwf1999@gmail.com>
2023-02-20 17:45:51 -0500

    added info for the models to create and set up connections between them in the index.js file

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'category_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: 'category_id'
+});
 
 module.exports = {
   Product,
@@ -18,3 +31,5 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+// TODO: not sure if I did the foreignKey's correctly. Will need to check with a tutor about that
